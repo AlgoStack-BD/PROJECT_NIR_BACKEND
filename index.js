d5c65e3f1ba466860459dd29034576a41dfb9118
mahi1212 <mahi121.mr@gmail.com>
2023-07-16 13:16:42 +0600

    initial setup done

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uq4tjid.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        // const database = client.db('razor-cut-app');
+        console.log('Connected to server');
+        
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Nir listening at server : http://localhost:${port}`)
+})
