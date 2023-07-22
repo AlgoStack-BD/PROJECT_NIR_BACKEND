884e20b1244ce00a59220b37b4abe47741e8a7f0
mahi1212 <mahi121.mr@gmail.com>
2023-07-22 13:11:19 +0600

    opt verification done

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors")
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectId } = require('mongodb');
 require('dotenv').config()
+const { randomBytes } = require('crypto'); // For generating OTP
+const nodemailer = require('nodemailer');
+
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -12,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uq4tjid.mongodb.net/?retryWrites=true&w=majority`;
+// Initialize SendGrid with your API key
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -21,10 +25,20 @@ const client = new MongoClient(uri, {
     }
 });
 
+// Create a nodemailer transporter with your email service configuration
+const transporter = nodemailer.createTransport({
+    service: 'Gmail', // Replace with your email service (e.g., Gmail, Outlook, etc.)
+    auth: {
+        user: process.env.NODE_MAILER_USER,
+        pass: process.env.NODE_MAILER_PASS
+    }
+});
+
 async function run() {
     try {
         const database = client.db('nir');
         const usersCollection = database.collection('users');
+        const otpCollection = database.collection('otp');
 
         // create user
         app.post('/create-user', async (req, res) => {
@@ -132,62 +146,84 @@ async function run() {
                 })
             }
         })
-        // app.patch('/update-user/:id', async (req, res) => {
-        //     const { id } = req.params;
-        //     const data = req.body; // Assuming the data to update is sent in the request body
-        //     console.log(data)
-        //     try {
-        //         // Check if the provided ID is a valid ObjectId
-        //         if (!ObjectId.isValid(id)) {
-        //             return res.status(400).json({
-        //                 status: 400,
-        //                 message: "Invalid user ID"
-        //             });
-        //         }
 
-        //         // Build the update object based on the fields available in the request body
-        //         const updateObject = {};
-        //         if (data.name) updateObject.name = data.name;
-        //         if (data.email) updateObject.email = data.email;
-        //         if (data.password) updateObject.password = data.password;
-        //         if (data.phone) updateObject.phone = data.phone;
-        //         if (data.isVerified !== undefined) updateObject.isVerified = data.isVerified;
-        //         if (data.image) updateObject.image = data.image;
-        //         if (data.location) updateObject.location = data.location;
-        //         if (data.totalPost) updateObject.totalPost = data.totalPost;
-        //         if (data.rentSuccess) updateObject.rentSuccess = data.rentSuccess;
-        //         console.log("Update Object:", updateObject);
-        //         // Update the user data in the MongoDB collection
-        //         const result = await usersCollection.updateOne(
-        //             { _id: new ObjectId(id) },
-        //             { $set: updateObject }
-        //         );
-        //         console.log(result);
-        //         if (result.modifiedCount === 0) {
-        //             return res.status(404).json({
-        //                 status: 404,
-        //                 message: "User not found"
-        //             });
-        //         }
+        // send verification code to email
+        app.post('/getVerificationCode', async (req, res) => {
+            const { email } = req.body;
 
-        //         res.json({
-        //             status: 200,
-        //             message: "User updated successfully"
-        //         });
-        //     } catch (err) {
-        //         console.error(err);
-        //         res.status(500).json({
-        //             status: 500,
-        //             message: "Internal Server Error"
-        //         });
-        //     }
-        // });
+            // Generate a random 4-digit OTP
+            const otp = Math.floor(1000 + Math.random() * 9000).toString();
+
+            // Compose the email to send
+            const mailOptions = {
+                to: email, // The recipient's email address
+                subject: 'OTP Verification Code',
+                text: `Your OTP verification code is: ${otp}`,
+            };
+
+            try {
+                // Save the generated OTP along with the user's email and a timestamp to the database
+                const otpData = {
+                    email: email,
+                    otp: otp,
+                    timestamp: new Date()
+                };
+                await otpCollection.insertOne(otpData);
+
+                // Send the email using the nodemailer transporter
+                await transporter.sendMail(mailOptions);
+                res.json({
+                    status: 200,
+                    message: "OTP sent successfully",
+                    // otp: otp // For testing purposes, you can send the OTP back in the response
+                });
+            } catch (err) {
+                console.error(err);
+                res.json({
+                    status: 500,
+                    message: "Failed to send OTP"
+                });
+            }
+        });
+
+        // Route to verify the user-provided OTP against the last generated OTP
+        app.post('/verifyOTP', async (req, res) => {
+            const { email, userOTP } = req.body;
+
+            // Find the last OTP generated for the provided email address
+            const query = { email: email };
+            // Sort by timestamp in descending order to get the latest OTP
+            const sort = { timestamp: -1 };
+            const lastOTPData = await otpCollection.findOne(query, { sort: sort });
+
+            if (!lastOTPData) {
+                return res.json({
+                    status: 404,
+                    message: "No OTP found for the provided email"
+                });
+            }
+
+            // Compare the user-provided OTP with the last generated OTP
+            if (userOTP === lastOTPData.otp) {
+                res.json({
+                    status: 200,
+                    message: "OTP verification successful"
+                });
+            } else {
+                res.json({
+                    status: 400,
+                    message: "Invalid OTP"
+                });
+            }
+        });
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
